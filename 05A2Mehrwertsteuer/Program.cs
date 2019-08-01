c7c3673331cd4bf2aaca877490f2ecab496830b9
frank paulsen <frobnic+ita@gmail.com>
2019-08-01 13:16:53 +0200

    Projektdateien hinzufügen.

---
+++ 05A2Mehrwertsteuer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05A2Mehrwertsteuer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
